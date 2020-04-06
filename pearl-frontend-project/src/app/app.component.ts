755093613b99874281fef2133702dd84f333cb68
Tanya Zhong <tanyazhong1@gmail.com>
2020-04-06 13:09:27 -0700

    table and pipe

--- pearl-frontend-project/src/app/app.component.ts
+++ pearl-frontend-project/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import {Message} from './message';
-import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { Message } from './message';
+//import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 const URL = "https://3ikr11e11g.execute-api.us-east-1.amazonaws.com/dev/interns/message/get";
 
@@ -16,25 +16,22 @@ const URL = "https://3ikr11e11g.execute-api.us-east-1.amazonaws.com/dev/interns/
 
 export class AppComponent implements OnInit {
   title = 'pearl-frontend-project';
-  messages: Message[] = [];
-  displayedColumns = ["seqNo", "description", "duration"];
-  constructor(private httpClient: HttpClient) { }
+  dataSource;
+  messages;
+  displayedColumns = ["Name", "Message", "Time"];
+  constructor(private http: HttpClient) { }
 
 
-  getMessages(): Observable<Message[]> {
-    return this.httpClient
-    .get(URL)
-    .map(response => {
-      const msgs = response.json();
-      return msgs.map((msg) => new Message(msg));
-    });
+  getMessages() {
+    return this.http
+      .get(URL)
   }
-  
+
   ngOnInit() {
     this.getMessages()
-      .subscribe((res: HttpResponse<Message[]>) => {
-          console.log(res);
-          this.messages = res.body;
+      .subscribe((res) => {
+        this.messages = res;
+        this.dataSource = this.messages.messages;
       });
   }
 
--- pearl-frontend-project/src/app/app.module.ts
+++ pearl-frontend-project/src/app/app.module.ts
@@ -2,21 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TruncatePipe } from './truncate.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TruncatePipe
   ],
   imports: [
     BrowserModule,
     MatTableModule,
+    MatPaginatorModule,
     HttpClientModule,
     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
